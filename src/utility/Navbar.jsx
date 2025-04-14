216da95a473fb5ca43c85831f7dde286bf29d57a
alokraj-05 <alok200504@gmail.com>
2025-04-14 22:53:59 +0530

    import remove

--- src/utility/Navbar.jsx
+++ src/utility/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import logo from "../assets/Sergio_log.png";
-import github from "../assets/github.png";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
